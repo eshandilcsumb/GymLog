a2f61f9e3d2db751e0a731e2e436974c4c62505e
eshandilcsumb <eshandil@csumb.edu>
2024-12-06 14:43:53 -0800

    Completed video 10. Login screen uses database on a non-main thread to verify credentials. Updated user DAO to have predicated query to fetch user details if the credentials exists. Need to resolve issue of waiting for the credential thread to return with user information on login screen. This will be done in the next video.

--- app/src/main/java/com/example/gymlog/database/GymLogRepository.java
+++ app/src/main/java/com/example/gymlog/database/GymLogRepository.java
@@ -79,4 +79,21 @@ public class GymLogRepository {
         });
     }
 
+    public User getUserByUsername(String username) {
+        Future<User> future = GymLogDatabase.databaseWriteExecutor.submit(
+                new Callable<User>() {
+                    @Override
+                    public User call() throws Exception {
+                        return userDAO.getUserByUserName(username);
+                    }
+                });
+
+        try {
+            future.get();
+        } catch (InterruptedException | ExecutionException e) {
+            Log.i(MainActivity.TAG, "Problem while getting all gym logs from the repository.");
+        }
+
+        return null;
+    }
 }
